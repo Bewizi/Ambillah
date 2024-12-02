0ec66bf64e7e82f988e91a2139e117279eb7626f
Bewizi <toluwalase.bewaji@yahoo.com>
2024-12-02 12:32:29 +0100

    worked on tablet responsiveness

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-[#141414] mt-32">
+  <footer class="bg-[#141414] mt-32 md:px-5 lg:px-0">
     <header class="max-w-7xl mx-auto py-10">
       <section class="flex flex-col items-center justify-center">
         <div class="mb-5">
@@ -14,7 +14,7 @@
         </nav>
       </section>
       <hr class="mt-10" />
-      <div class="mx-auto flex flex-col lg:flex-row items-center justify-between mt-8">
+      <div class="mx-auto flex flex-col md:flex-row items-center justify-between mt-8">
         <p class="mb-5 lg:mb-0">&copy; 2024 Ambillah. All rights reserved</p>
         <div class="space-x-5">
           <i class="pi pi-instagram bg-gray-900 p-2 rounded-full"></i>
--- src/views/Home/component/MarqueeSlider.vue
+++ src/views/Home/component/MarqueeSlider.vue
@@ -6,7 +6,7 @@ const helloArray = ['Evolve', 'Evolve', 'Evolve', 'Evolve', 'Evolve', 'Evolve']
 </script>
 
 <template>
-  <div class="container max-w-7xl mx-auto mt-32 px-5 lg:px-8 h-0">
+  <div class="container max-w-7xl mx-auto mt-32 px-5 md:px-8 h-0">
     <Vue3Marquee>
       <span v-for="(word, index) in helloArray" :key="index" class="mr-12 text-6xl">
         {{ word }}
@@ -14,7 +14,7 @@ const helloArray = ['Evolve', 'Evolve', 'Evolve', 'Evolve', 'Evolve', 'Evolve']
     </Vue3Marquee>
   </div>
 
-  <section class="max-w-4xl mx-auto mt-32 lg:px-8">
+  <section class="max-w-4xl mx-auto mt-32 md:px-8 lg:px-8">
     <div class="linear">
       <div class="flex flex-col items-center justify-center font-Poppins">
         <h2 class="text-4xl font-bold mb-10 text-center">Reimagine Your Brand's Future</h2>
