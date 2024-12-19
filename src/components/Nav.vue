54fca3243d977cd946e73879a572e527c2756ed7
Bewizi <toluwalase.bewaji@yahoo.com>
2024-12-19 14:55:17 +0100

    made the navbar fixed when  scroll down to a certian viewport

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,7 +4,7 @@ import { onMounted, onUnmounted, ref } from 'vue'
 
 const isOpen = ref(false)
 const isActive = ref('Home')
-const isScrolled = ref(false)
+const isSticky = ref(true)
 
 const toggleNavbar = () => {
   isOpen.value = !isOpen.value
@@ -20,104 +20,133 @@ const getActiveLink = (route: string) => {
     : ''
 }
 
-const handleIntersection = (entrires: IntersectionObserverEntry[]) => {
-  isScrolled.value = !entrires[0].isIntersecting
-}
+// let observer: IntersectionObserver | null = null
+
+// const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+//   isSticky.value = !entries[0].isIntersecting
+// }
+//
+// onMounted(() => {
+//   observer = new IntersectionObserver(handleIntersection, {
+//     root: null,
+//     threshold: 0.1,
+//   })
+//
+//   const target = document.querySelector('header')
+//   if (target) observer.observe(target)
+// })
+// onUnmounted(() => {
+//   if (observer) {
+//     observer.disconnect()
+//   }
+// })
 
 onMounted(() => {
-  const observer = new IntersectionObserver(handleIntersection, {
-    root: null,
-    threshold: 0.1,
-  })
-
-  const target = document.querySelector('#hero-section')
-  if (target) observer.observe(target)
-
+  const sentinel = document.querySelector('#sentinel-header')
+  const obsever = new IntersectionObserver(
+    ([entries]) => {
+      isSticky.value = !entries.isIntersecting
+    },
+    { root: null, threshold: 0.1 },
+  )
+  if (sentinel) {
+    obsever.observe(sentinel)
+  }
   onUnmounted(() => {
-    if (target) observer.unobserve(target)
+    if (sentinel) {
+      obsever.unobserve(sentinel)
+    }
   })
 })
 </script>
 
 <template>
-  <header :class="[isScrolled ? 'bg-neutral-900 sticky z-50' : 'bg-neutral-900/50']">
-    <div class="hidden md:block">
-      <section class="max-w-4xl mx-auto flex items-center justify-between md:px-4 py-8">
-        <div>
-          <figure>
-            <img alt="" src="/images/logo.png" />
-          </figure>
-        </div>
-
-        <nav class="flex items-center justify-center gap-10 font-Poppins font-light">
-          <router-link :class="getActiveLink('Home')" to="/" @click="updateActive('Home')"
-            >Home
-          </router-link>
-          <router-link to="/">Our services</router-link>
-          <router-link
-            :class="getActiveLink('Careers')"
-            to="/about"
-            @click="updateActive('Careers')"
-            >Careers</router-link
-          >
-          <router-link
-            :class="getActiveLink('Portfolio')"
-            to="/portfolio"
-            @click="updateActive('Portfolio')"
-            >Our portfolio</router-link
-          >
-        </nav>
-
-        <div>
-          <Button class="bg-[#C90202] hover:bg-[#C90202]/65 text-white py-5">Book us</Button>
-        </div>
-      </section>
-    </div>
-
-    <div class="block md:hidden">
-      <section class="flex items-center justify-between px-4 py-4">
-        <div>
-          <figure>
-            <img alt="" src="/images/logo.png" />
-          </figure>
-        </div>
-
-        <section class="flex items-center gap-5">
+  <div id="sentinel-header">
+    <header
+      :class="[
+        isSticky
+          ? 'bg-neutral-900/50 fixed top-0 w-full z-50 shadow-black shadow-lg'
+          : 'relative bg-neutral-900/50',
+      ]"
+    >
+      <div class="hidden md:block">
+        <section class="max-w-4xl mx-auto flex items-center justify-between md:px-4 py-8">
           <div>
-            <Button class="bg-[#C90202] hover:bg-[#C90202]/65 text-white">Book us</Button>
+            <figure>
+              <img alt="" src="/images/logo.png" />
+            </figure>
           </div>
-          <!--  icon -->
-          <div class="flex items-center">
-            <i v-if="!isOpen" class="pi pi-bars hidden" @click="toggleNavbar"></i>
-            <i v-else class="pi pi-times" @click="toggleNavbar"></i>
-          </div>
-          <!--  icon -->
-        </section>
 
-        <nav
-          :class="{ 'h-[40vh] opacity-100': isOpen, 'h-0 opacity-0 overflow-hidden': !isOpen }"
-          class="flex flex-col items-start pl-5 justify-start gap-6 font-Poppins font-light bg-[#070D04]/50 absolute inset-x-0 top-16 transition-all duration-300 z-10"
-        >
-          <router-link :class="getActiveLink('Home')" to="/" @click="updateActive('Home')"
-            >Home</router-link
+          <nav class="flex items-center justify-center gap-10 font-Poppins font-light">
+            <router-link :class="getActiveLink('Home')" to="/" @click="updateActive('Home')"
+              >Home
+            </router-link>
+            <router-link to="/">Our services</router-link>
+            <router-link
+              :class="getActiveLink('Careers')"
+              to="/about"
+              @click="updateActive('Careers')"
+              >Careers</router-link
+            >
+            <router-link
+              :class="getActiveLink('Portfolio')"
+              to="/portfolio"
+              @click="updateActive('Portfolio')"
+              >Our portfolio</router-link
+            >
+          </nav>
+
+          <div>
+            <Button class="bg-[#C90202] hover:bg-[#C90202]/65 text-white py-5">Book us</Button>
+          </div>
+        </section>
+      </div>
+
+      <div class="block md:hidden">
+        <section class="flex items-center justify-between px-4 py-4">
+          <div>
+            <figure>
+              <img alt="" src="/images/logo.png" />
+            </figure>
+          </div>
+
+          <section class="flex items-center gap-5">
+            <div>
+              <Button class="bg-[#C90202] hover:bg-[#C90202]/65 text-white">Book us</Button>
+            </div>
+            <!--  icon -->
+            <div class="flex items-center">
+              <i v-if="!isOpen" class="pi pi-bars hidden" @click="toggleNavbar"></i>
+              <i v-else class="pi pi-times" @click="toggleNavbar"></i>
+            </div>
+            <!--  icon -->
+          </section>
+
+          <nav
+            :class="{ 'h-[40vh] opacity-100': isOpen, 'h-0 opacity-0 overflow-hidden': !isOpen }"
+            class="flex flex-col items-start pl-5 justify-start gap-6 font-Poppins font-light bg-[#070D04]/50 absolute inset-x-0 top-16 transition-all duration-300 z-10"
           >
-          <router-link to="">Our services</router-link>
-          <router-link
-            :class="getActiveLink('Careers')"
-            to="/about"
-            @click="updateActive('Careers')"
-            >Careers</router-link
-          >
-          <router-link
-            :class="getActiveLink('Portfolio')"
-            to="/portfolio"
-            @click="updateActive('Portfolio')"
-            >Our portfolio</router-link
-          >
-        </nav>
-      </section>
-    </div>
-  </header>
+            <router-link :class="getActiveLink('Home')" to="/" @click="updateActive('Home')"
+              >Home</router-link
+            >
+            <router-link to="">Our services</router-link>
+            <router-link
+              :class="getActiveLink('Careers')"
+              to="/about"
+              @click="updateActive('Careers')"
+              >Careers</router-link
+            >
+            <router-link
+              :class="getActiveLink('Portfolio')"
+              to="/portfolio"
+              @click="updateActive('Portfolio')"
+              >Our portfolio</router-link
+            >
+          </nav>
+        </section>
+      </div>
+    </header>
+  </div>
 </template>
 
 <style scoped>
