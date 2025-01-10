916dc89ceb2daca2f41e0247d7114a46a1e39eff
Bewizi <toluwalase.bewaji@yahoo.com>
2025-01-10 19:27:44 +0100

    tried adding  smooth scrool to the servicesection couldn't figure it out

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home/HomeView.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/About/AboutView.vue'),
+  },
+  {
+    path: '/portfolio',
+    name: 'Portfolio',
+    component: () => import('../views/Portfolio/PortfolioView.vue'),
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/About/AboutView.vue'),
-    },
-    {
-      path: '/portfolio',
-      name: 'Portfolio',
-      component: () => import('../views/Portfolio/PortfolioView.vue'),
-    },
-  ],
+  // history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    if (to.hash) {
+      const element = document.getElementById(to.hash)
+      if (element) {
+        return {
+          el: to.hash,
+          behavior: 'smooth',
+        }
+      }
+    }
+    return { top: 0 }
+  },
 })
 
 export default router
