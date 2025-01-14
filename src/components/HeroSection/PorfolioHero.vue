007f28c2e6f019f8f88db8d1f20e0adac43311b2
Bewizi <toluwalase.bewaji@yahoo.com>
2025-01-15 00:45:24 +0100

    Raleway font-family added to the hero section h1

--- src/components/HeroSection/PorfolioHero.vue
+++ src/components/HeroSection/PorfolioHero.vue
@@ -4,7 +4,7 @@
   <section class="hero h-[100vh] bg-cover bg-center bg-[url('/images/img_15.png')]">
     <div class="max-w-4xl mx-auto px-5 lg:px-0">
       <div class="flex flex-col items-center justify-center text-center h-[100vh]">
-        <h1 class="text-4xl md:text-6xl lg:text-7xl font-bold">
+        <h1 class="text-4xl md:text-6xl lg:text-7xl font-bold font-Raleway">
           Some of the stellar <br />
           projects we have done
         </h1>
